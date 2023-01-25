cd8072dc7b957a64553f9afecb049ed9a6a12f62
ajin3830 <ajin3830@gmail.com>
2023-01-25 12:00:27 -0500

    added languages array

--- src/index.js
+++ src/index.js
@@ -22,11 +22,30 @@ document.addEventListener("DOMContentLoaded", () => {
  function fetchBehindName(submittedName) {
      fetch(`https://www.behindthename.com/api/lookup.json?name=${submittedName}&key=ju257858736`)
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => renderBehindName(data))
  }
 
- 
+ function renderBehindName(data) {
+    let usageLang = []
+    data.forEach( name => {
+        const usages = name.usages.map(usage => usage.usage_full)
+        console.log(usages)
+        usages.forEach( usage => usageLang.push(usage))
+    })
+    console.log(usageLang)
+
+ }
+
+// function renderBehindName(data) {
+    
+//     data.forEach(name => usageLang.push(usage.usage_full))
+//     console.log()
+// }
+
 // language, gender inside a table using iteration
+// array1: name male
+// array2: name female
+// array3: name 
 
 
 
