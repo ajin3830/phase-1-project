31accb306b22695a7d76e5da069c9ecd618d98f5
ajin3830 <ajin3830@gmail.com>
2023-01-24 21:05:47 -0500

    added function fetchbehindname

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-
 const searchedname = document.querySelector("#card-title");
 const predictedage = document.querySelector("#predicted-age")
-
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
     //     let submittedname = e.
-        fetchagify(e.target["name-search"].value)
-
-      console.log(e);
+        fetchAgify(e.target["name-search"].value)
+        fetchBehindName(e.target["name-search"].value)
+        console.log(e);
     
     });
 });
 
-
-
-
- function fetchagify(submittedname) {
- fetch(`https://api.agify.io?name=${submittedname}`)
+ function fetchAgify(submittedName) {
+    fetch(`https://api.agify.io?name=${submittedName}`)
     .then(response => response.json())
-    .then(data => renderageprediction(data))
+    .then(data => console.log(data))
+ 
+ }
+ 
+ function fetchBehindName(submittedName) {
+     fetch(`https://www.behindthename.com/api/lookup.json?name=${submittedName}&key=ju257858736`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
  }
 
+ 
+// language, gender inside a table using iteration
 
 
 
