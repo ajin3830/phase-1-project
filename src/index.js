018dc644f6a3f087040ded2a5eeac9064974e082
Julian Patterson <julian.patterson@gmail.com>
2023-01-25 18:06:09 -0500

    This batch contains our MVP app with successful API calls to Agify and BehindTheName, hidden divs, and properly formatted return data

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ const searchedName = document.querySelector("#card-title");
 const predictedAge = document.querySelector("#age-prediction")
 const nameCount = document.querySelector("#name-count");
 const nameUsageLang = document.querySelector("#name-usage-languages");
+const predictedAgeSentence = document.querySelector("#predicted-age-sentence");
+//let displayDivs= false;
+const behindNameDiv = document.querySelector(".hide");
+const behindNameShow = document.querySelector(".name-information-cards");
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
-    //     let submittedname = e.
+        document.querySelector("#name-usage-languages").innerHTML = "";
         fetchAgify(e.target["name-search"].value)
         fetchBehindName(e.target["name-search"].value)
     });
 });
 
+    function displayBehindTheName() {  
+        console.log(behindNameDiv)
+        console.log(behindNameShow)
+        if (behindNameDiv.className === "hide") {
+            behindNameDiv.className = "name-information-cards-container"
+    }}
+
+
  function fetchAgify(submittedName) {
     fetch(`https://api.agify.io?name=${submittedName}`)
     .then(response => response.json())
-    .then(data => renderAgify(data))
+    .then(data => {
+        renderAgify(data);
+    })
  }
   
 function renderAgify(data) {
@@ -38,6 +52,8 @@ function renderAgify(data) {
 
  function renderBehindName(data) {
     // let usageLang = []
+    //newDiv = document.createElement('div')
+    //layout.appendChild(newDiv)
     data.forEach( name => {
         const usages = name.usages.map(usage => usage.usage_full)
         console.log(usages)
@@ -49,6 +65,7 @@ function renderAgify(data) {
         })
 
     })
+    displayBehindTheName()
 }       
 
 
